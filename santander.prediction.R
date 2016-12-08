8d2ac5ccd732b652b813e21b6998f48f960298a4
Vetal1977 <vitaly.bezgachev@gmail.com>
2016-12-08 06:00:50 +0100

    Rename result file

--- santander.prediction.R
+++ santander.prediction.R
@@ -97,4 +97,4 @@ result <- get.result.df(test)
 result_write <- prepare.result.to.write(result)
 
 # save to csv
-write.csv(result_write, 'result45.csv', quote = FALSE, row.names = FALSE)
+write.csv(result_write, 'result47.csv', quote = FALSE, row.names = FALSE)
